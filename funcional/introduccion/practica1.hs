df77ef8513de3a5574317a59840c93d7b722415f
Aneth Guadalupe Ochoa Lozano <71891002+AnethOchoa@users.noreply.github.com>
2020-11-13 23:36:49 -0700

    Create practica1.hs

---
+++ funcional/introduccion/practica1.hs
@@ -0,0 +1,30 @@
+--SUMA
+suma1 a b = a + b
+suma2 a b c d e = a + b + c + d + e
+--RESTA
+resta1 x y = x - y
+resta2 x y z = (x - y) - z
+--MULTIPLICACION
+multi1 a b = a * b
+multi2 a b c = (a * b) * c
+--DIVISION
+d1 a b = a / b
+div1 a b = a `div` b
+--RESIDUO
+residuo1 a b = a `mod` b
+residuo2 x y z = x `mod` y `mod` z
+--TRUE FALSE
+tf1 x = x == "FALSE"  
+tf2 y = y == "TRUE"   
+-- AND - OR
+and1 a b = a && b
+or2 c d = c || d
+-- COOMPARACION
+comp1 a b = a == b
+comp2 a b = a /= b
+-- NOT
+pregunta1 a = not a
+pregunta2 a b = not (a && b)
+--CONCATENAR
+con1 a b = a ++ b
+con2 a b c = a ++ b ++ c 
