1dad076efa78a6877652b14269c971e6ca68b39b
Aneth Guadalupe Ochoa Lozano <71891002+AnethOchoa@users.noreply.github.com>
2021-01-16 01:00:05 -0700

    Las reglas en prolog

--- funcional/Logica/holamundo.pl
+++ funcional/Logica/holamundo.pl
@@ -8,3 +8,15 @@ progenitor(jose,ana).
 progenitor(jose,patricia).
 progenitor(tomas,isabel).
 progenitor(patricia,jaime).
+hombre(jose).
+hombre(tomas).
+hombre(jaime).
+mujer(clara).
+mujer(isabel).
+mujer(ana).
+mujer(patricia).
+
+%tia
+tia(X,Y):-progenitor(Z,X), progenitor(Z, A), progenitor(A, Y), mujer(X).
+%abuela
+abuela(X,Y):- progenitor(X,Z), progenitor(Z,Y), mujer(X).
